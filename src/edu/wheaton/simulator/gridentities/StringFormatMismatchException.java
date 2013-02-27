d0fd837ffeb3ad9f4ad39034e8d48439aaf7cef3
brandon-garcia <brandon.garcia@my.wheaton.edu>
2013-02-26 20:51:07 -0600

    nothing done here

--- src/edu/wheaton/simulator/gridentities/StringFormatMismatchException.java
+++ src/edu/wheaton/simulator/gridentities/StringFormatMismatchException.java
@@ -2,6 +2,11 @@ package edu.wheaton.simulator.gridentities;
 
 public class StringFormatMismatchException extends Exception {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -1901675785258553919L;
+
 	public StringFormatMismatchException() {
 		super();
 	}
