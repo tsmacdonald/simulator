22c5cf00f354ca25434a2cb2e166bb4ee92b806a
akonwi <akonwi@gmail.com>
2013-03-03 23:56:29 -0600

    adding a Database class for development

---
+++ src/edu/wheaton/simulator/statistics/Database.java
@@ -0,0 +1,14 @@
+package edu.wheaton.simulator.statistics;
+
+import com.google.common.collect.Table;
+
+public class Database implements Table{
+
+	/**
+	 * TODO: Implement Table interface
+	 * TODO: Make a table of Snapshots
+	 * TODO: Create instance of ImmutableTable from table
+	 * TODO: Outside access to database will refer to ImmutableTable 
+	 * 		 to ensure database is not changed
+	 */
+}
