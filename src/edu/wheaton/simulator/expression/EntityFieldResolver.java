f193f1c944ca73174fa821f2c9ffae9502aaf655
brandon-garcia <brandon.garcia@my.wheaton.edu>
2013-03-15 21:08:50 -0500

    Cleaning/reworking Expression classes. In its current state Expression does not support the evaluation of say, "#{entity.fieldname}" if the Expression is not offered a variable with the name "entity.fieldname". This functionality is handled by the new EntityFieldResolver class and has yet to be fully implemented.
    I also updated the JavaDoc to reflect changes.
    
    I introduced some errors due to changing the Expression interface and will
    work to get that fixed as well as implementing the EntityFieldResolver methods

---
+++ src/edu/wheaton/simulator/expression/EntityFieldResolver.java
@@ -0,0 +1,39 @@
+package edu.wheaton.simulator.expression;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import net.sourceforge.jeval.VariableResolver;
+import net.sourceforge.jeval.function.FunctionException;
+import edu.wheaton.simulator.entity.Entity;
+
+public class EntityFieldResolver implements VariableResolver {
+
+	private Map<String, Entity> entityMap;
+
+	/*
+	 * default constructor
+	 */
+	public EntityFieldResolver() {
+		entityMap = new HashMap<String, Entity>();
+	}
+
+	/*
+	 * copy constructor
+	 */
+	public EntityFieldResolver(EntityFieldResolver resolver) {
+		entityMap.putAll(resolver.entityMap);
+	}
+
+	@Override
+	public String resolveVariable(String variableName)
+			throws FunctionException {
+		// TODO method stub
+		throw new FunctionException(variableName);
+	}
+
+	public void addEntity(String alias, Entity entity) {
+		entityMap.put(alias, entity);
+	}
+
+}
--- src/edu/wheaton/simulator/expression/Expression.java
+++ src/edu/wheaton/simulator/expression/Expression.java
@@ -1,33 +1,138 @@
 package edu.wheaton.simulator.expression;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-
 import net.sourceforge.jeval.EvaluationException;
 import net.sourceforge.jeval.Evaluator;
-import net.sourceforge.jeval.VariableResolver;
 import net.sourceforge.jeval.function.Function;
-import edu.wheaton.simulator.datastructure.Value;
 import edu.wheaton.simulator.entity.Entity;
 
 public final class Expression {
-	
-	public static Value evaluate(String expr, List<Function> functions, Map<String,Entity> entities, Map<String,Value> values) throws EvaluationException{
-		Evaluator evaluator = new Evaluator();
-		
-		VariableResolver varRes = new EntityResolver(entities);
-		evaluator.setVariableResolver(varRes);
-		
-		for(Function f : functions)
-			evaluator.putFunction(f);
-		
-		Set<Map.Entry<String,Value>> valueMapSet = values.entrySet();
-		for( Map.Entry<String,Value> entry : valueMapSet ){
-			evaluator.putVariable(entry.getKey(), entry.getValue().toString());
-		}
-	
-		return new Value(evaluator.evaluate(expr));
+
+	private Evaluator evaluator;
+	private EntityFieldResolver resolver;
+	private Object expr;
+
+	/*
+	 * Default constructor
+	 * 
+	 * The expression string is retrieved by calling expr.toString()
+	 */
+	public Expression(Object exprStr) {
+		setString(exprStr);
+		evaluator = new Evaluator();
+		resolver = new EntityFieldResolver();
+		evaluator.setVariableResolver(resolver);
 	}
-	
+
+	/*
+	 * Copy constructor
+	 */
+	public Expression(Expression expr) {
+		evaluator = new Evaluator();
+		evaluator.setFunctions(expr.evaluator.getFunctions());
+		evaluator.setVariables(expr.evaluator.getVariables());
+		resolver = new EntityFieldResolver(expr.resolver);
+		evaluator.setVariableResolver(resolver);
+		setString(expr.expr);
+	}
+
+	public void setString(Object exprStr) {
+		this.expr = exprStr;
+	}
+
+	/*
+	 * @Param name Do not format this String as you must do when creating an
+	 * expression String. Simply pass the desired variable name.
+	 * 
+	 * @Param valueIsString Denotes whether the value should be interpreted as
+	 * a String rather than of Numeric or Boolean type
+	 */
+	public void addVariable(String name, String value) {
+		evaluator.putVariable(name, value);
+	}
+
+	/*
+	 * @Param aliasName The name used to refer to the Entity in the expression
+	 * String ("this", "other", etc.)
+	 */
+	public void addEntity(String aliasName, Entity entity) {
+		resolver.addEntity(aliasName, entity);
+	}
+
+	public void addFunctions(Function... functions) {
+		for (Function f : functions)
+			evaluator.putFunction(f);
+	}
+
+	/*
+	 * Formats the passed variable name into the format required by the parser.
+	 * 
+	 * Variables contained in an expression string must be properly formatted
+	 * in order to be correctly evaluated by the parser. Do not use this
+	 * function on the 'name' parameter passed to the method 'addVariable(...)'
+	 * 
+	 * @Param name The variable's name
+	 * 
+	 * @Return The formatted string
+	 */
+	public static String formatName(String name) {
+		return "#{" + name + "}";
+	}
+
+	/*
+	 * Formats the passed String value into the format required by the parser.
+	 * 
+	 * Values that are intended to be parsed as type String require a special
+	 * format
+	 * 
+	 * @Param value
+	 * 
+	 * @Return The formatted string
+	 */
+	public static String formatString(String value) {
+		return "'" + value + "'";
+	}
+
+	/*
+	 * clear all added variables
+	 */
+	public void clearVariables() {
+		evaluator.clearVariables();
+	}
+
+	/*
+	 * clear all added functions
+	 */
+	public void clearFunctions() {
+		evaluator.clearFunctions();
+	}
+
+	public Boolean evaluateBool() throws EvaluationException {
+		return evaluator.getBooleanResult(expr.toString());
+	}
+
+	public Double evaluateDouble() throws EvaluationException {
+		return evaluator.getNumberResult(expr.toString());
+	}
+
+	public String evaluateString() throws EvaluationException {
+		return evaluator.evaluate(expr.toString());
+	}
+
+	public static Boolean evaluateBool(Object exprStr)
+			throws EvaluationException {
+		Evaluator evaluator = new Evaluator();
+		return evaluator.getBooleanResult(exprStr.toString());
+	}
+
+	public static Double evaluateDouble(Object exprStr)
+			throws EvaluationException {
+		Evaluator evaluator = new Evaluator();
+		return evaluator.getNumberResult(exprStr.toString());
+	}
+
+	public static String evaluateString(Object exprStr)
+			throws EvaluationException {
+		Evaluator evaluator = new Evaluator();
+		return evaluator.evaluate(exprStr.toString());
+	}
 }
