f7bb42f8efb7e5df07159c6b4699cebbc46e5922
Willy McHie <wmchie2@gmail.com>
2013-03-21 21:23:40 -0500

    modified EditEntityScreen to match new structure.

--- src/edu/wheaton/simulator/gui/EditEntityScreen.java
+++ src/edu/wheaton/simulator/gui/EditEntityScreen.java
@@ -31,33 +31,9 @@ public class EditEntityScreen extends Screen {
 	 */
 	private static final long serialVersionUID = 4021299442173260142L;
 	
-	private JTabbedPane tabs;
-	
-	private JPanel lowerPanel;
-	
-	private JPanel generalPanel;
-	
-	private JLabel generalLabel;
-	
-	private JPanel fieldPanel;
-	
-	private JPanel fieldPanel2;
-	
-	private JLabel fieldLabel;
-	
-	private JPanel triggerPanel;
-	
-	private JPanel triggerPanel2;
-
-	private JLabel triggerLabel;
-	
-	private JLabel nameLabel;
-	
 	private JTextField nameField;
 	
 	private JColorChooser colorTool;
-	
-	private JButton loadIconButton;
 	
 	private ArrayList<JTextField> fieldNames; 
 	
@@ -69,14 +45,6 @@ public class EditEntityScreen extends Screen {
 	
 	private ArrayList<JButton> fieldDeleteButtons;
 	
-	private JLabel fieldNameLabel;
-	
-	private JLabel fieldValueLabel;
-	
-	private JLabel fieldTypeLabel;
-	
-	private JButton addFieldButton;
-	
 	private ArrayList<JTextField> triggerNames;
 	
 	private ArrayList<JTextField> triggerPriorities;
@@ -87,48 +55,33 @@ public class EditEntityScreen extends Screen {
 	
 	private ArrayList<JButton> triggerDeleteButtons;
 	
-	private JLabel triggerNameLabel;
-	
-	private JLabel triggerPriorityLabel;
-	
-	private JLabel triggerConditionLabel;
-	
-	private JLabel triggerResultLabel;
-	
-	private JButton addTriggerButton;
-	
-	private JButton cancelButton;
-	
-	private JButton finishButton;
-	
 	public EditEntityScreen(ScreenManager sm) {
 		super(sm);
-		tabs = new JTabbedPane();
-		lowerPanel = new JPanel();
-		generalPanel = new JPanel();
-		fieldPanel = new JPanel();
-		fieldPanel2 = new JPanel();
+		JTabbedPane tabs = new JTabbedPane();
+		JPanel lowerPanel = new JPanel();
+		JPanel generalPanel = new JPanel();
+		JPanel fieldPanel = new JPanel();
+		JPanel fieldPanel2 = new JPanel();
 		fieldPanel2.setLayout(new GridLayout(2, 4));
-		triggerPanel = new JPanel();
-		triggerPanel2 = new JPanel();
+		JPanel triggerPanel = new JPanel();
+		JPanel triggerPanel2 = new JPanel();
 		triggerPanel2.setLayout(new GridLayout(2, 5));
-		generalLabel = new JLabel("General Info");
+		JLabel generalLabel = new JLabel("General Info");
 		generalLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		generalLabel.setHorizontalTextPosition(SwingConstants.CENTER);
-		fieldLabel = new JLabel("Field Info");
+		JLabel fieldLabel = new JLabel("Field Info");
 		fieldLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		fieldLabel.setHorizontalTextPosition(SwingConstants.CENTER);
-		triggerLabel = new JLabel("Trigger Info");
+		JLabel triggerLabel = new JLabel("Trigger Info");
 		triggerLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		triggerLabel.setHorizontalTextPosition(SwingConstants.CENTER);
-		nameLabel = new JLabel("Name: ");
+		JLabel nameLabel = new JLabel("Name: ");
 		nameField = new JTextField();
 		colorTool = new JColorChooser();
-		loadIconButton = new JButton("Load icon");
-		//single label at the top of each column \/
-		fieldNameLabel = new JLabel("Field Name");
-		fieldValueLabel = new JLabel("Field Initial Value");
-		fieldTypeLabel = new JLabel("Field Type");
+		JButton loadIconButton = new JButton("Load icon");
+		JLabel fieldNameLabel = new JLabel("Field Name");
+		JLabel fieldValueLabel = new JLabel("Field Initial Value");
+		JLabel fieldTypeLabel = new JLabel("Field Type");
 		fieldNames = new ArrayList<JTextField>();
 		fieldNames.add(new JTextField());
 		fieldValues = new ArrayList<JTextField>();
@@ -137,11 +90,11 @@ public class EditEntityScreen extends Screen {
 		fieldTypes.add(new JComboBox(typeNames));
 		fieldDeleteButtons = new ArrayList<JButton>();
 		fieldDeleteButtons.add(new JButton("Delete"));
-		addFieldButton = new JButton("Add Field");
-		triggerNameLabel = new JLabel("Trigger Name");
-		triggerPriorityLabel = new JLabel("Trigger Priority");
-		triggerConditionLabel = new JLabel("Trigger Condition");
-		triggerResultLabel = new JLabel("Trigger Result");
+		JButton addFieldButton = new JButton("Add Field");
+		JLabel triggerNameLabel = new JLabel("Trigger Name");
+		JLabel triggerPriorityLabel = new JLabel("Trigger Priority");
+		JLabel triggerConditionLabel = new JLabel("Trigger Condition");
+		JLabel triggerResultLabel = new JLabel("Trigger Result");
 		triggerNames = new ArrayList<JTextField>();
 		triggerNames.add(new JTextField());
 		triggerPriorities = new ArrayList<JTextField>();
@@ -153,9 +106,9 @@ public class EditEntityScreen extends Screen {
 		triggerResults.add(new JTextField());
 		triggerDeleteButtons = new ArrayList<JButton>();
 		triggerDeleteButtons.add(new JButton("Delete"));
-		addTriggerButton = new JButton("Add Trigger");
-		cancelButton = new JButton("Cancel");
-		finishButton = new JButton("Finish");
+		JButton addTriggerButton = new JButton("Add Trigger");
+		JButton cancelButton = new JButton("Cancel");
+		JButton finishButton = new JButton("Finish");
 		
 		lowerPanel.add(cancelButton);
 		lowerPanel.add(finishButton);
@@ -190,14 +143,9 @@ public class EditEntityScreen extends Screen {
 		tabs.addTab("Fields", fieldPanel);
 		tabs.addTab("Triggers",  triggerPanel);
 		
-		components = new JComponent[2];
-		components[0] = tabs;
-		components[1] = lowerPanel;
-	}
+		this.add(tabs);
+		this.add(lowerPanel);
 
-
-	public void addComponents(JPanel panel) {
-		panel.add(label);
 	}
 
 	@Override
