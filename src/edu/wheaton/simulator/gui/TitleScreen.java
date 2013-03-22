43d829835f5d2a52b8351df5c29db4625a0d1858
Willy McHie <wmchie2@gmail.com>
2013-03-22 07:42:41 -0500

    random tweaks

--- src/edu/wheaton/simulator/gui/TitleScreen.java
+++ src/edu/wheaton/simulator/gui/TitleScreen.java
@@ -25,22 +25,28 @@ public class TitleScreen extends Screen {
 		JLabel label = new JLabel("Welcome to the Simulator!");
 		label.setHorizontalAlignment(SwingConstants.CENTER);
 		label.setPreferredSize(new Dimension(300, 150));
-		JPanel panel = new JPanel();
-		panel.setLayout(new BoxLayout(panel, BoxLayout.PAGE_AXIS));
+		JPanel mainPanel = new JPanel();
+		mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
+		JPanel panel1 = new JPanel();
+		JPanel panel2 = new JPanel();
 		JButton newSim = new JButton("New Simulation");
 		newSim.setAlignmentX(CENTER_ALIGNMENT);
-		newSim.setPreferredSize(new Dimension(100, 60));
+		newSim.setPreferredSize(new Dimension(200, 70));
 		newSim.addActionListener(this);
 		JButton loadSim = new JButton("Load a saved Simulation");
 		loadSim.setAlignmentX(CENTER_ALIGNMENT);
-		loadSim.setPreferredSize(new Dimension(100, 60));
+		loadSim.setPreferredSize(new Dimension(200, 70));
 		// Since serialization is not yet implemented.
 		loadSim.setEnabled(false);
 		loadSim.addActionListener(this);
 		this.add(label, BorderLayout.NORTH);
-		panel.add(newSim);
-		panel.add(loadSim);
-		this.add(panel, BorderLayout.CENTER);
+		panel1.add(newSim);
+		panel2.add(loadSim);
+		mainPanel.add(Box.createVerticalGlue());
+		mainPanel.add(panel1);
+		mainPanel.add(panel2);
+		mainPanel.add(Box.createVerticalGlue());
+		this.add(mainPanel, BorderLayout.CENTER);
 
 		this.setVisible(true);
 	}
