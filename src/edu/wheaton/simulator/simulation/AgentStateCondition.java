a1bdb270aa8a32f553cee1243ff3549586993b5c
brandon-garcia <brandon.garcia@my.wheaton.edu>
2013-03-22 00:30:59 -0500

    removed a superfluous TODO and corrected the formatting of another

--- src/edu/wheaton/simulator/simulation/AgentStateCondition.java
+++ src/edu/wheaton/simulator/simulation/AgentStateCondition.java
@@ -76,8 +76,6 @@ public class AgentStateCondition extends EndCondition {
 		Iterator<Slot> it = this.getGrid().iterator();
 
 		// TODO: Get the Agent team to add names to Agents
-		// TODO: Get the Agent team to get the Field class figured out and
-		// provide a means for accessing a Field's value
 		while (it.hasNext()) {
 			Slot s = it.next();
 
