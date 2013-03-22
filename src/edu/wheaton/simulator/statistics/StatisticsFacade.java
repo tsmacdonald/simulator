7552cb08081819661e12bd10004cc094e7b709fd
ArcticPanda <nico.lasta@my.wheaton.edu>
2013-03-22 17:15:15 -0500

    Starting to work on a facade that the GUI team can work with.

---
+++ src/edu/wheaton/simulator/statistics/StatisticsFacade.java
@@ -0,0 +1,20 @@
+/**
+ * StatisticsFacade.java
+ * 
+ * Facade that takes different information
+ * and returns it to the GUI team. 
+ * 
+ * @author Nico Lasta
+ * Wheaton College
+ * CSCI 335
+ * Spring 2013
+ */
+
+package edu.wheaton.simulator.statistics;
+
+import edu.wheaton.simulator.entity.Agent;
+import 
+
+public class StatisticsFacade {
+
+}
