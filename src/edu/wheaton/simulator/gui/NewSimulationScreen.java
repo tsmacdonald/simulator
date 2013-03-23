1b615dec0a3c0b76a37812f58a155a69b6a15575
ian.walling <ian.walling@my.wheaton.edu>
2013-03-23 18:40:17 -0500

    Trying to commit

--- src/edu/wheaton/simulator/gui/NewSimulationScreen.java
+++ src/edu/wheaton/simulator/gui/NewSimulationScreen.java
@@ -4,11 +4,8 @@ import java.awt.BorderLayout;
 import java.awt.Canvas;
 import java.awt.Color;
 import java.awt.Dimension;
-<<<<<<< HEAD
 import java.awt.Graphics;
 import java.awt.GridLayout;
-=======
->>>>>>> df7dffc5ea6e1ee0d05988e2d4296e29558fb498
 import java.awt.event.ActionEvent;
 import java.awt.image.ImageObserver;
 
