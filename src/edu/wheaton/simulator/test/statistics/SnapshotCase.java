d9dacd4da6b5da606a15d0834a36b5c9f5483fc0
akonwi <akonwi@gmail.com>
2013-03-24 14:40:57 -0500

    minor tweaks

---
+++ src/edu/wheaton/simulator/test/statistics/SnapshotCase.java
@@ -0,0 +1,32 @@
+package edu.wheaton.simulator.test.statistics;
+
+import static org.junit.Assert.fail;
+import junit.framework.Assert;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import edu.wheaton.simulator.statistics.FieldSnapshot;
+
+public class SnapshotCase {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		//fail("Not yet implemented");
+	}
+
+	@Test
+	public void fieldSnapshotTest() {
+		FieldSnapshot snap = new FieldSnapshot("name", "akon");
+		Assert.assertNotNull(snap);
+	}
+}
