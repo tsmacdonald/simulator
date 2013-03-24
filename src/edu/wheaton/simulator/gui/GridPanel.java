59629cef88b6ef4e46ee47eb9f8541850f402f7d
ian.walling <ian.walling@my.wheaton.edu>
2013-03-24 16:09:22 -0500

    Grid made theoretically with Agents.

--- src/edu/wheaton/simulator/gui/GridPanel.java
+++ src/edu/wheaton/simulator/gui/GridPanel.java
@@ -1,10 +1,12 @@
 package edu.wheaton.simulator.gui;
 
 import java.awt.Color;
-import java.awt.Component;
 import java.awt.Graphics;
+import javax.swing.JPanel;
+import edu.wheaton.simulator.entity.Agent;
+import edu.wheaton.simulator.simulation.GUIToAgentFacade;
 
-public class GridPanel extends Component {
+public class GridPanel extends JPanel {
 
 	private static final long serialVersionUID = -20L;
 
@@ -13,6 +15,10 @@ public class GridPanel extends Component {
 	private int width;
 	
 	private int height;
+
+	private int gridWidth;
+	
+	private int gridHeight;
 	
 	public GridPanel(ScreenManager sm) {
 		this.sm = sm;
@@ -21,11 +27,11 @@ public class GridPanel extends Component {
 	public void paint(Graphics g) {
 		width = this.getWidth();
 		height = this.getHeight();
-		int gridWidth = sm.getGUIwidth();
-		int gridHeight = sm.getGUIheight();
+		gridWidth = sm.getGUIwidth();
+		gridHeight = sm.getGUIheight();
 		int pixelWidth = width / gridWidth;
 		int pixelHeight = height / gridHeight;
-		int squareSize = Math.min(pixelWidth, pixelHeight) - 1;
+		int squareSize = Math.min(pixelWidth, pixelHeight);
 		for (int i = 0; i < gridWidth; i++) {
 			for (int j = 0; j < gridHeight; j++) {
 				g.drawRect(squareSize * i, squareSize * j, 
@@ -34,9 +40,23 @@ public class GridPanel extends Component {
 		}
 	}
 
-	public void agentPaint(){
+	public void agentPaint(Graphics g){
 		width = this.getWidth();
 		height = this.getHeight();
-		
+		gridWidth = sm.getGUIwidth();
+		gridHeight = sm.getGUIheight();
+		int pixelWidth = width / gridWidth;
+		int pixelHeight = height / gridHeight;
+		int squareSize = Math.min(pixelWidth, pixelHeight) - 1;
+		for (int i = 0; i < gridWidth; i++) {
+			for (int j = 0; j < gridHeight; j++) {
+				Agent a = sm.getFacade().getAgent(i, j);
+				if(a instanceof Agent){
+					g.setColor(a.getColor());
+					g.fillRect(squareSize * i, squareSize * j, 
+							   squareSize, squareSize);
+				}
+			}
+		}
 	}
 }
