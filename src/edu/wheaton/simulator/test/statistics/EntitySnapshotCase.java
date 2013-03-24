6a6fc278a3cabdf5a5d375f550405dd13349cd08
ArcticPanda <nico.lasta@my.wheaton.edu>
2013-03-24 15:28:09 -0500

    Creating a JUnit Test for EntitySnapshot

---
+++ src/edu/wheaton/simulator/test/statistics/EntitySnapshotCase.java
@@ -0,0 +1,32 @@
+package edu.wheaton.simulator.test.statistics;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import edu.wheaton.simulator.entity.Entity;
+import edu.wheaton.simulator.statistics.EntitySnapshot;
+
+public class EntitySnapshotCase {
+
+	@Before
+	public void setUp() throws Exception {
+		Entity entity = new Entity();
+		
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		//fail("Not yet implemented");
+	}
+	
+	@Test
+	public void entitySnapshotTest() {
+		EntitySnapshot entity = new EntitySnapshot
+	}
+
+}
