dedea9bda695eac3022aa101382523ff56459275
dwgill <daniel.gill@my.wheaton.edu>
2013-03-24 23:36:29 -0500

    Updated the obersveGrid method.

--- src/edu/wheaton/simulator/statistics/GridObserver.java
+++ src/edu/wheaton/simulator/statistics/GridObserver.java
@@ -23,21 +23,42 @@ public class GridObserver {
 		this.statManager = statManager;
 	}
 
-	public void observe(Grid grid, Integer step, Collection<Prototype> prototypes) { 
+	/**
+	 * Record a step of the simulation. 
+	 * @param grid The grid at the point in time corresponding to step. 
+	 * @param step The point in the simulation being recorded. 
+	 * @param prototypes
+	 */
+	public void recordSimulationStep(Grid grid, Integer step, Collection<Prototype> prototypes) { 
+		
+		for (Prototype prototype : prototypes) { 
+			statManager.addPrototypeSnapshot(SnapshotFactory.makePrototypeSnapshot(prototype, step));
+		}
+		
 		for (Slot s : grid) { 
-			EntitySnapshot slotSnap = SnapshotFactory.makeSlotSnapshot(s, step);
-			statManager.addGridEntity(slotSnap);
-			Agent agent = s.getAgent();
-			if (agent == null) 
-				continue;
-			AgentSnapshot agentSnap = SnapshotFactory.makeAgentSnapshot(agent, step);
-			statManager.addGridEntity(agentSnap);
-			for (String currentPrototypeName : prototypes.keySet()) { 
-				PrototypeSnapshot currentSnapshot;
-				currentSnapshot = SnapshotFactory.makePrototypeSnapshot(
-						prototypes.get(currentPrototypeName), step);
+			statManager.addGridEntity(SnapshotFactory.makeSlotSnapshot(s, step));
+			Agent agent; 
+			if ((agent = s.getAgent()) != null) { 
+				statManager.addGridEntity(SnapshotFactory.makeAgentSnapshot(agent, step));
 			}
-			// TODO Make sure this method is completely implemented! (for the most part)
+		}
+		
+		
+		
+		for (Slot s : grid) { 
+//			EntitySnapshot slotSnap = SnapshotFactory.makeSlotSnapshot(s, step);
+//			statManager.addGridEntity(slotSnap);
+//			Agent agent = s.getAgent();
+//			if (agent == null) 
+//				continue;
+//			AgentSnapshot agentSnap = SnapshotFactory.makeAgentSnapshot(agent, step);
+//			statManager.addGridEntity(agentSnap);
+//			for (String currentPrototypeName : prototypes.keySet()) { 
+//				PrototypeSnapshot currentSnapshot;
+//				currentSnapshot = SnapshotFactory.makePrototypeSnapshot(
+//						prototypes.get(currentPrototypeName), step);
+//			}
+//			// TODO Make sure this method is completely implemented! (for the most part)
 		}
 	}
 }
