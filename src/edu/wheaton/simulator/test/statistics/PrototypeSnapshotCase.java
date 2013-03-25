63ccebbc6f0ebc4c475ab163c02facda215c44f0
ArcticPanda <nico.lasta@my.wheaton.edu>
2013-03-25 13:07:23 -0500

    Finished PrototypeSnapshotCase.java

--- src/edu/wheaton/simulator/test/statistics/PrototypeSnapshotCase.java
+++ src/edu/wheaton/simulator/test/statistics/PrototypeSnapshotCase.java
@@ -1,7 +1,16 @@
 package edu.wheaton.simulator.test.statistics;
 
+/**
+ * A JUnit test case for testing PrototypeSnapshot.java.
+ * 
+ * @author Nico Lasta
+ * Wheaton College, CSCI 335
+ * Spring 2013
+ * 25 Mar 2013
+ */
 import java.util.HashMap;
-import java.util.HashSet;
+
+import junit.framework.Assert;
 
 import org.junit.After;
 import org.junit.Before;
@@ -11,38 +20,56 @@ import com.google.common.collect.ImmutableSet;
 
 import edu.wheaton.simulator.entity.AgentID;
 import edu.wheaton.simulator.entity.Prototype;
-import edu.wheaton.simulator.statistics.FieldSnapshot;
+import edu.wheaton.simulator.simulation.Grid;
 import edu.wheaton.simulator.statistics.PrototypeSnapshot;
 import edu.wheaton.simulator.statistics.SnapshotFactory;
 
 public class PrototypeSnapshotCase {
 	
 	String categoryName;
+	Grid grid;
 	Prototype prototype;
 	HashMap<String, String> fields;
 	int population;
 	ImmutableSet<AgentID> children;
 	Integer step;
 
+	/**
+	 * Initialize variables.
+	 */
 	@Before
 	public void setUp() throws Exception {
+		categoryName = "testing";
+		grid = new Grid(10, 10);
+		prototype = new Prototype(grid, "tester");
+		fields = new HashMap<String, String>();
+		population = 50;
 		children = prototype.childIDs();
+		step = new Integer(23);
 	}
 
+	/**
+	 * Give variables null pointers.
+	 */
 	@After
 	public void tearDown() throws Exception {
 	}
 
+	/**
+	 * Auto-generated method stub.
+	 */
 	@Test
 	public void test() {
 		//fail("Not yet implemented");
 	}
 	
+	/**
+	 * Tests to make sure a PrototypeSnapshot object was successfully created.
+	 */
 	public void prototypeSnapshotTest() {
 		PrototypeSnapshot protoSnap = new PrototypeSnapshot(categoryName, prototype.getPrototypeID(),
 				SnapshotFactory.makeFieldSnapshots(fields), population,
 				children, step);
-		
+		Assert.assertNotNull("PrototypeSnapshot not created.", protoSnap);
 	}
-
 }
