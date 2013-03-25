94688d630308e89d9654f870128541e770408634
brandon-garcia <brandon.garcia@my.wheaton.edu>
2013-03-25 00:06:03 -0500

    added documentation to Expression.java(implementation of ExpressionEvaluator.java) and AbstractExpressionFunction(partial implementation of ExpressionFunction.java). I also added a 'clearEntities' method to the ExpressionEvaluator interface.

--- src/edu/wheaton/simulator/expression/AbstractExpressionFunction.java
+++ src/edu/wheaton/simulator/expression/AbstractExpressionFunction.java
@@ -18,48 +18,95 @@ public abstract class AbstractExpressionFunction implements ExpressionFunction {
 	protected static final int RESULT_TYPE_BOOL = FunctionConstants.FUNCTION_RESULT_TYPE_NUMERIC;
 	protected static final int RESULT_TYPE_STRING = FunctionConstants.FUNCTION_RESULT_TYPE_STRING;
 	
+	/*
+	 * A wrapper constructed from a JEval Evaluator instance that is not known until the point at which the
+	 * function is called by JEval. For now it is null.
+	 */
 	private ExpressionEvaluator evaluator=null;
 	
+	/**
+	 * The name of the function as it is to appear in an Expression (ex: "add")
+	 */
 	@Override
 	public abstract String getName();
 
+	/**
+	 * Returns one of the three constants defined at the top of this class
+	 */
 	@Override
 	public abstract int getResultType();
 
+	/**
+	 * When this method is called from within an Expression, it's parameters are evaluated
+	 * down to either a string ("'i'm a string'"), a boolean ("true"), or a double ("1.0" or "1")
+	 * and then passed to this function which then performs the intended logic
+	 */
 	@Override
 	public abstract String execute(String[] args) throws EvaluationException;
 
+	/**
+	 * returns the wrapper for the JEval Evaluator that is null until JEval calls the function
+	 * and provides a means to construct an ExpressionEvaluator
+	 * @return
+	 */
 	private ExpressionEvaluator getExprEval(){
 		return evaluator;
 	}
 	
+	/**
+	 * Returns the Entity mapped to the given 'aliasName'
+	 * 
+	 * @param aliasName
+	 * 		the name used to refer to the entity in the expression
+	 */
 	@Override
 	public Entity resolveEntity(String aliasName) {
 		return getExprEval().getEntity(aliasName.replaceAll("'", ""));
 	}
 	
+	/**
+	 * Same as resolveEntity except that the return value is casted
+	 * to Agent
+	 */
 	@Override
 	public Agent resolveAgent(String aliasName){
 		return (Agent)resolveEntity(aliasName);
 	}
 
+	/**
+	 * Returns the variable value (doubleStr,stringStr, or boolStr) mapped to the given 'aliasName'
+	 * 
+	 * @param aliasName
+	 * 		the name used to refer to the variable in the expression
+	 */
 	@Override
 	public String resolveVariable(String variableName)
 			throws EvaluationException {
 		return getExprEval().getVariableValue(variableName);
 	}
 
+	/**
+	 * Formats this object into a form that is usable by JEval such that
+	 * the layer of abstraction is not lost.
+	 */
 	@Override
 	public Function toJEvalFunction() {
 		final AbstractExpressionFunction xEnclosingWrapper = this;
 		return new Function() {
 
+			/*
+			 * used to differentiate between ExpressionFunction.execute() and Function.execute()
+			 */
 			protected final AbstractExpressionFunction enclosingWrapper = xEnclosingWrapper;
 
 			@Override
 			public FunctionResult execute(Evaluator evaluator, String arguments)
 					throws FunctionException {
 				
+				/*
+				 * wraps the passed 'Evaluator' into an ExpressionEvaluator to provide a higher level
+				 * of abstraction away from the internals of JEval
+				 */
 				setExprEval(new Expression(evaluator,(Expression.EntityFieldResolver)evaluator.getVariableResolver()));
 				
 				String[] args = arguments.split(",");
@@ -79,6 +126,10 @@ public abstract class AbstractExpressionFunction implements ExpressionFunction {
 		};
 	}
 
+	/**
+	 * Used to set the ExpressionEvaluator once constructed
+	 * @param evaluator
+	 */
 	private void setExprEval(ExpressionEvaluator evaluator) {
 		this.evaluator = evaluator;
 	}
