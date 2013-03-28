67ed682879bfbc83b4771bdbf47d5687811b9ee6
brandon-garcia <brandon.garcia@my.wheaton.edu>
2013-03-28 01:37:38 -0500

    added error messages to GetFieldOfAgentAt.java. The reason RPStest currently fails is because the method attempted to get a field from a null agent reference (grid.getAgent(x,y) returned null)

--- src/edu/wheaton/simulator/expression/GetFieldOfAgentAt.java
+++ src/edu/wheaton/simulator/expression/GetFieldOfAgentAt.java
@@ -1,5 +1,7 @@
 package edu.wheaton.simulator.expression;
 
+import edu.wheaton.simulator.entity.Agent;
+import edu.wheaton.simulator.simulation.Grid;
 import net.sourceforge.jeval.EvaluationException;
 
 public class GetFieldOfAgentAt extends AbstractExpressionFunction {
@@ -18,8 +20,24 @@ public class GetFieldOfAgentAt extends AbstractExpressionFunction {
 	public String execute(String[] args) throws EvaluationException {
 		Double x = Double.valueOf(args[0]);
 		Double y = Double.valueOf(args[1]);
+		
+		Agent xThis = resolveAgent("this");
+		Grid grid = xThis.getGrid();
+		
+		if(grid.isValidCoord(x.intValue(), y.intValue())==false){
+			System.err.println("invalid coord sent as param to 'getFieldOfAgentAt(...)': [" + x + "," + y + "]");
+		}
+		
+		Agent target = grid.getAgent(x.intValue(), y.intValue());
+		
+		if(target==null){
+			System.err.println("grid.getAgent(x,y) returned null therefore no field can be retrieved");
+		}
+		
 		String fieldName = args[2].replaceAll("'", "");
-		Object fieldValue = resolveAgent("this").getGrid().getAgent(x.intValue(), y.intValue()).getFieldValue(fieldName);
+		
+		
+		Object fieldValue = target.getFieldValue(fieldName);
 		return fieldValue.toString();
 		}
 }
