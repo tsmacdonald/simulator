f9636871755e31e63facdfada21eb6b2e6650369
calebdemoss <caleb.demoss@my.wheaton.edu>
2013-04-01 18:14:53 -0500

    Added basic test suite

---
+++ src/edu/wheaton/simulator/test/gui/AllTests.java
@@ -0,0 +1,11 @@
+package edu.wheaton.simulator.test.gui;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ TestDisplay.class, TestGeneralButtonListener.class })
+public class AllTests {
+
+}
