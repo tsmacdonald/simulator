20b1fbaf571029ee69749ca461371a525879a9aa
brandon-garcia <brandon.garcia@my.wheaton.edu>
2013-04-02 17:12:07 -0500

    warning fixes

--- src/edu/wheaton/simulator/test/statistics/StatisticsManagerTest.java
+++ src/edu/wheaton/simulator/test/statistics/StatisticsManagerTest.java
@@ -170,7 +170,7 @@ public class StatisticsManagerTest {
 		sm.addPrototypeSnapshot(protoSnap); 
 		
 		try {
-			double result = sm.getAvgLifespan(protoSnap.id);
+			sm.getAvgLifespan(protoSnap.id);
 		}
 		catch (NameNotFoundException e) {
 			e.printStackTrace();
