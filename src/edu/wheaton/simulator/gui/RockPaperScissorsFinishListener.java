60e8d63eba121cc162eae97d23bddbcee1649552
Emmanuel Pederson <david.pederson@my.wheaton.edu>
2013-04-08 00:23:51 -0500

    Added initRockPaperScissors to the GUIToAgentFacade and made a listoner class so it can easily be added as a button to the GUI.

---
+++ src/edu/wheaton/simulator/gui/RockPaperScissorsFinishListener.java
@@ -0,0 +1,45 @@
+package edu.wheaton.simulator.gui;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JTextField;
+	
+public class RockPaperScissorsFinishListener implements ActionListener {
+		
+		private JTextField name;
+		private JTextField width;
+		private JTextField height;
+		private Manager sm;
+		
+		public RockPaperScissorsFinishListener(JTextField name, JTextField width, JTextField height, 
+										  Manager sm){
+			this.name = name;
+			this.width = width;
+			this.height = height;
+			this.sm = sm;
+		}
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			try {
+				int heightInt = getHeight();
+				int widthInt = getWidth();
+				sm.setFacade(widthInt, heightInt);
+				sm.updateGUIManager(getName(), widthInt, heightInt);
+			} catch(java.lang.NumberFormatException nfe) { 
+				System.err.println("Invalid input passed to RockPaperScissorsListener");
+			}
+			Screen upload = sm.getScreen("Edit Simulation");
+			sm.getFacade().setPriorityUpdate();
+			sm.getFacade().initRockPaperScissors();
+			sm.update(upload);
+			sm.loadScreen(upload);
+		}
+		public String getName(){
+			return name.getText();
+		}
+		public int getHeight(){
+			return Integer.parseInt(height.getText());
+		}
+		public int getWidth(){
+			return Integer.parseInt(width.getText());
+		}
+}
