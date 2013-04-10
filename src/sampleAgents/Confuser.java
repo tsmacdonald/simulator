95f26ca8bda2d6ff9e5f85ebda14ccdda47a6ad7
Emmanuel Pederson <david.pederson@my.wheaton.edu>
2013-04-10 17:39:37 -0500

    Adds a confuser sample agent that is a bouncer that also will make the agents that are in an adjacent space change directions. In testing, I found that for this unit to work, the linear update should be selected in the grid setup screen.

---
+++ src/sampleAgents/Confuser.java
@@ -0,0 +1,80 @@
+package sampleAgents;
+
+import edu.wheaton.simulator.datastructure.ElementAlreadyContainedException;
+import edu.wheaton.simulator.entity.Prototype;
+import edu.wheaton.simulator.entity.Trigger;
+import edu.wheaton.simulator.expression.Expression;
+
+/**
+ * A confuser that bounces back and forth and will change the 
+ * direction of any agent with the fields xNextDirection and yNextDirection
+ * 
+ * @author David Emmanuel
+ *
+ */
+public class Confuser extends SampleAgent {
+	public Confuser(){
+		super.sampleAgents.add(this);
+	}
+	
+	@Override
+	public Prototype initSampleAgent(Prototype confuser) {
+		return initBouncer(confuser);
+	}
+	
+	private static Prototype initBouncer(Prototype confuser){
+		// Add fields
+		try {
+			confuser.addField("xNextDirection", 0 + "");
+			confuser.addField("yNextDirection", 1 + "");
+			confuser.addField("temp", 0+"");
+		} catch (ElementAlreadyContainedException e) {
+			e.printStackTrace();
+		}
+		
+		// openSpot conditionals
+		Expression freeSpot = new Expression("isSlotOpen(this.x + this.xNextDirection, this.y + this.yNextDirection)");
+		Expression notFreeSpot = new Expression("!isSlotOpen(this.x + this.xNextDirection, this.y + this.yNextDirection)");
+
+		// Move behavior
+		Expression move = new Expression("move('this', this.x + this.xNextDirection, this.y + this.yNextDirection)");
+		
+		// Confuse the agent (change their direction)
+		Expression confuse1 = new Expression("setFieldOfAgent('this', this.x + 1, this.y +1, 'xNextDirection', round(this.xNextDirection * cos(PI/4) - this.yNextDirection * sin(PI/4)))"
+				+ " || setFieldOfAgent('this', this.x + 1, this.y +1, 'yNextDirection', round(this.temp * sin(PI/4) + this.yNextDirection * cos(PI/4)))");
+		Expression confuse2 = new Expression("setFieldOfAgent('this', this.x + 1, this.y, 'xNextDirection', round(this.xNextDirection * cos(PI/4) - this.yNextDirection * sin(PI/4)))"
+				+ " || setFieldOfAgent('this', this.x + 1, this.y, 'yNextDirection', round(this.temp * sin(PI/4) + this.yNextDirection * cos(PI/4)))");
+		Expression confuse3 = new Expression("setFieldOfAgent('this', this.x + 1, this.y -1, 'xNextDirection', round(this.xNextDirection * cos(PI/4) - this.yNextDirection * sin(PI/4)))"
+				+ " || setFieldOfAgent('this', this.x + 1, this.y -1, 'yNextDirection', round(this.temp * sin(PI/4) + this.yNextDirection * cos(PI/4)))");
+		Expression confuse4 = new Expression("setFieldOfAgent('this', this.x, this.y +1, 'xNextDirection', round(this.xNextDirection * cos(PI/4) - this.yNextDirection * sin(PI/4)))"
+				+ " || setFieldOfAgent('this', this.x, this.y +1, 'yNextDirection', round(this.temp * sin(PI/4) + this.yNextDirection * cos(PI/4)))");
+		Expression confuse5 = new Expression("setFieldOfAgent('this', this.x, this.y -1, 'xNextDirection', round(this.xNextDirection * cos(PI/4) - this.yNextDirection * sin(PI/4)))"
+				+ " || setFieldOfAgent('this', this.x, this.y -1, 'yNextDirection', round(this.temp * sin(PI/4) + this.yNextDirection * cos(PI/4)))");
+		Expression confuse6 = new Expression("setFieldOfAgent('this', this.x - 1, this.y +1, 'xNextDirection', round(this.xNextDirection * cos(PI/4) - this.yNextDirection * sin(PI/4)))"
+				+ " || setFieldOfAgent('this', this.x - 1, this.y +1, 'yNextDirection', round(this.temp * sin(PI/4) + this.yNextDirection * cos(PI/4)))");
+		Expression confuse7 = new Expression("setFieldOfAgent('this', this.x - 1, this.y, 'xNextDirection', round(this.xNextDirection * cos(PI/4) - this.yNextDirection * sin(PI/4)))"
+				+ " || setFieldOfAgent('this', this.x -1, this.y, 'yNextDirection', round(this.temp * sin(PI/4) + this.yNextDirection * cos(PI/4)))");
+		Expression confuse8 = new Expression("setFieldOfAgent('this', this.x - 1, this.y -1, 'xNextDirection', round(this.xNextDirection * cos(PI/4) - this.yNextDirection * sin(PI/4)))"
+				+ " || setFieldOfAgent('this', this.x - 1, this.y -1, 'yNextDirection', round(this.temp * sin(PI/4) + this.yNextDirection * cos(PI/4)))");
+		
+		// turn clockwise
+		Expression bounce = new Expression("setField('this', 'xNextDirection', -1 * this.xNextDirection) ||" +
+				"setField('this', 'yNextDirection', -1 * this.yNextDirection)");
+		
+		confuser.addTrigger(new Trigger("confuse", 1, new Expression("TRUE"), confuse1));
+		confuser.addTrigger(new Trigger("confuse", 1, new Expression("TRUE"), confuse2));
+		confuser.addTrigger(new Trigger("confuse", 1, new Expression("TRUE"), confuse3));
+		confuser.addTrigger(new Trigger("confuse", 1, new Expression("TRUE"), confuse4));
+		confuser.addTrigger(new Trigger("confuse", 1, new Expression("TRUE"), confuse5));
+		confuser.addTrigger(new Trigger("confuse", 1, new Expression("TRUE"), confuse6));
+		confuser.addTrigger(new Trigger("confuse", 1, new Expression("TRUE"), confuse7));
+		confuser.addTrigger(new Trigger("confuse", 1, new Expression("TRUE"), confuse8));
+
+		confuser.addTrigger(new Trigger("bounce", 1, notFreeSpot, bounce));
+		confuser.addTrigger(new Trigger("move", 1, freeSpot, move));
+		
+		Prototype.addPrototype(confuser);
+
+		return confuser;
+	}
+}
