82c0e168e02e1ed5071f4903699b6f4549db01a1
ElliotPenson <elliotpenson@gmail.com>
2013-04-10 19:08:59 -0500

    Code clean up
    - Added some comments
    - General refactoring
    - Moved getCustomFieldMap() to GridEntity and Grid from Entity. This way, the method can be defined differently these two classes.

--- src/edu/wheaton/simulator/simulation/SimulationPauseException.java
+++ src/edu/wheaton/simulator/simulation/SimulationPauseException.java
@@ -2,9 +2,6 @@ package edu.wheaton.simulator.simulation;
 
 public class SimulationPauseException extends Exception {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 2100980371782493609L;
 
 	public SimulationPauseException(String errorMessage) {
