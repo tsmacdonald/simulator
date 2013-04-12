9ff6addebc957905207d54ebf8b030e28daf6eb7
Grant Hensel <andrew.grant.hensel@gmail.com>
2013-04-11 19:45:34 -0500

    Housekeeping changes to account for changes in data stored by Prototypes

--- src/edu/wheaton/simulator/test/statistics/AgentSnapshotTableCase.java
+++ src/edu/wheaton/simulator/test/statistics/AgentSnapshotTableCase.java
@@ -54,7 +54,7 @@ public class AgentSnapshotTableCase {
 			}
 			ids.add(agent.getID());
 			for(int s = 1; s < 3; s++) {
-				snaps.add(new AgentSnapshot(agent.getID(), SnapshotFactory.makeFieldSnapshots(agent.getCustomFieldMap()), s, prototype.getName()));
+				snaps.add(new AgentSnapshot(agent.getID(), SnapshotFactory.makeFieldSnapshots(agent.getCustomFieldMap()), s, prototype.getName(), null));
 			}
 		}
 		table = null;
