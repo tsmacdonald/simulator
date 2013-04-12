6832a41f587900a77e79b3363563b524b5c4a7dc
ArcticPanda <nico.lasta@my.wheaton.edu>
2013-04-11 20:59:57 -0500

    Created BehaviorSnapshotCase.java, a JUnit test case for BehaviorSnapshots.

---
+++ src/edu/wheaton/simulator/test/statistics/BehaviorSnapshotCase.java
@@ -0,0 +1,39 @@
+package edu.wheaton.simulator.test.statistics;
+
+/**
+ * A JUnit test case for testing BehaviorSnapshot.java.
+ * 
+ * @author Nico Lasta
+ * Wheaton College, CSCI 335
+ * Spring 2013
+ * 11 Apr 2013
+ */
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import edu.wheaton.simulator.behavior.AbstractBehavior;
+import edu.wheaton.simulator.entity.Agent;
+
+@RunWith(JUnit4.class)
+public class BehaviorSnapshotCase {
+	
+	Agent actor, recipient;
+	AbstractBehavior behavior;
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+	}
+
+}
