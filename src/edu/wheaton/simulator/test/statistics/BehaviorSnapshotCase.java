a4e0846f27105edf5cd453096261afe503959b28
ArcticPanda <nico.lasta@my.wheaton.edu>
2013-04-12 14:55:57 -0500

    Merge remote-tracking branch 'refs/remotes/origin/behaviorsnapshotcase'
    Conflicts:
    	src/edu/wheaton/simulator/test/statistics/BehaviorSnapshotCase.java

--- src/edu/wheaton/simulator/test/statistics/BehaviorSnapshotCase.java
+++ src/edu/wheaton/simulator/test/statistics/BehaviorSnapshotCase.java
@@ -9,6 +9,8 @@ package edu.wheaton.simulator.test.statistics;
  * 11 Apr 2013
  */
 
+import junit.framework.Assert;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -16,16 +18,30 @@ import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
 import edu.wheaton.simulator.behavior.AbstractBehavior;
+import edu.wheaton.simulator.behavior.MoveBehavior;
+import edu.wheaton.simulator.datastructure.Grid;
 import edu.wheaton.simulator.entity.Agent;
+import edu.wheaton.simulator.entity.Prototype;
+import edu.wheaton.simulator.statistics.BehaviorSnapshot;
 
 @RunWith(JUnit4.class)
 public class BehaviorSnapshotCase {
-	
+
+	Grid grid;
+	Prototype prototype;
 	Agent actor, recipient;
 	AbstractBehavior behavior;
+	Integer step;
 
 	@Before
 	public void setUp() throws Exception {
+		grid = new Grid(1, 1);
+		prototype = new Prototype(grid, "BehaviorSnapshotTest");
+		actor = prototype.createAgent();
+		recipient = prototype.createAgent();
+		behavior = new MoveBehavior();
+		step = 23; // arbitrary
+
 	}
 
 	@After
@@ -33,7 +49,12 @@ public class BehaviorSnapshotCase {
 	}
 
 	@Test
-	public void test() {
+	public void behaviorSnapshotTest() {
+		BehaviorSnapshot behaviorSnap = new BehaviorSnapshot(actor.getID(),
+				behavior, recipient.getID(), step);
+		Assert.assertNotNull("BehaviorSnapshot not created", behaviorSnap);
+		System.out.println(behaviorSnap.serialize());
+		
 	}
 
 }
