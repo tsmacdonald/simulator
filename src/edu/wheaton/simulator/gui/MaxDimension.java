11c978a315204845fc384cc564612eaeaa550aeb
brandon-garcia <brandon.garcia@my.wheaton.edu>
2013-04-13 15:49:07 -0500

    refactoring gui

---
+++ src/edu/wheaton/simulator/gui/MaxDimension.java
@@ -0,0 +1,24 @@
+package edu.wheaton.simulator.gui;
+
+import java.awt.Dimension;
+
+public class MaxDimension extends Dimension {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 7913321320078340541L;
+
+	public MaxDimension(Dimension d) {
+		super(d);
+		// TODO Auto-generated constructor stub
+	}
+
+	public MaxDimension(int width, int height) {
+		super(width, height);
+		// TODO Auto-generated constructor stub
+	}
+	
+	public static final MaxDimension NULL = null;
+
+}
---
+++ src/edu/wheaton/simulator/gui/PrefDimension.java
@@ -0,0 +1,24 @@
+package edu.wheaton.simulator.gui;
+
+import java.awt.Dimension;
+
+public class PrefDimension extends Dimension {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -2422463360040406589L;
+
+	public PrefDimension(Dimension d) {
+		super(d);
+		// TODO Auto-generated constructor stub
+	}
+
+	public PrefDimension(int width, int height) {
+		super(width, height);
+		// TODO Auto-generated constructor stub
+	}
+	
+	public static final PrefDimension NULL = null;
+
+}
