4771b7b628b54702ba8b3cac753e1513b74bb364
Grant Hensel <andrew.grant.hensel@gmail.com>
2013-04-18 15:04:44 -0500

    Finished the savePrototype() method

--- src/edu/wheaton/simulator/statistics/Saver.java
+++ src/edu/wheaton/simulator/statistics/Saver.java
@@ -33,19 +33,19 @@ public class Saver {
 	 * Since PrototypeSnapshots are immutable, this collection is the same for each step
 	 */
 	private Map<String, PrototypeSnapshot> prototypes; 
-	
+
 	/**
 	 * The width of the grid we're saving
 	 */
 	private int width; 
-	
+
 	/**
 	 * The height of the grid we're saving
 	 */
 	private int height; 
-	
+
 	/**
-	 * Handels the ending conditions for the simulation
+	 * Handles the ending conditions for the simulation
 	 */
 	private SimulationEnder simEnder; 
 
@@ -56,7 +56,6 @@ public class Saver {
 	 */
 	public Saver(AgentSnapshotTable table, Map<String, PrototypeSnapshot> prototypes, 
 			int width, int height, SimulationEnder simEnder){
-		this.sb = new StringBuilder(); 
 		this.table = table; 
 		this.prototypes = prototypes; 
 		this.width = width; 
@@ -72,16 +71,18 @@ public class Saver {
 	 * @param filename The name of the file that's going to be saved
 	 */
 	public void saveSimulation(String filename){
+		sb = new StringBuilder(); 
+
 		//Name the file, first
 		filename = filename + ".txt";
-		
+
 		int currentStep = getCurrentStep();  
 		ImmutableMap<AgentID, AgentSnapshot> snaps = table.getSnapshotsAtStep(currentStep); 
-		
+
 		//Save the Grid dimensions
 		sb.append(width + "\n"); 
 		sb.append(height + "\n");  
-		
+
 		//Serialize and write all PrototypeSnapshots to file
 		for(PrototypeSnapshot proto : prototypes.values()){
 			sb.append(proto.serialize() + "\n"); 
@@ -91,7 +92,7 @@ public class Saver {
 		for(AgentSnapshot snap : snaps.values()){
 			sb.append(snap.serialize() + "\n"); 
 		}
-		
+
 		//Save the Ending Conditions
 		sb.append(simEnder.serialize()); 
 
@@ -108,13 +109,29 @@ public class Saver {
 		//What just got saved to file?
 		System.out.println("The following text was just saved to SimulationState.txt: \n" + sb); // TODO Delete
 	}
-	
+
 	/**
 	 * Create a save file for an individual prototype
 	 * @param proto
 	 */
 	public void savePrototype(Prototype proto){
-		SnapshotFactory.makePrototypeSnapshot(prototype); 
+		sb = new StringBuilder(); 
+		PrototypeSnapshot protoSnap = SnapshotFactory.makePrototypeSnapshot(proto);
+		sb.append(protoSnap.serialize()); 
+
+		String filename = proto.getName() + ".txt"; 
+		//Create BufferedWriter and BufferedReader
+		try {	
+			BufferedWriter writer = new BufferedWriter(new FileWriter(filename));
+			writer.write(sb.toString());
+			writer.close();
+		} catch (IOException e) {
+			System.err.println("Saver.java: IOException");
+			e.printStackTrace();
+		}
+		
+		//What just got saved to file?
+		System.out.println("The following text was just saved to " + filename + ": \n" + sb);
 	}
 
 	/**
