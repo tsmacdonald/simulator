7d707b4d114682bb0edbca15f172ff5f5c29634c
ElliotPenson <elliotpenson@gmail.com>
2013-04-20 19:36:05 -0500

    clearAll in simulator now doesn't take a prototype name

--- src/edu/wheaton/simulator/simulation/Simulator.java
+++ src/edu/wheaton/simulator/simulation/Simulator.java
@@ -323,7 +323,7 @@ public class Simulator {
 	 * 
 	 * @param a
 	 */
-	public void clearAll(String prototypeName) {
+	public void clearAll() {
 		for(int x = 0; x < getWidth(); x++) 
 			for(int y = 0; y < getHeight(); y++)
 				removeAgent(x, y);
