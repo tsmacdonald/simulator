345c94c2d3e91009f7df157ec22c2d157e6cd978
ElliotPenson <elliotpenson@gmail.com>
2013-04-20 19:34:57 -0500

    Change the names of some load/save methods and also cleared prototypes and notified grid on loading simulations

--- src/edu/wheaton/simulator/datastructure/Grid.java
+++ src/edu/wheaton/simulator/datastructure/Grid.java
@@ -206,7 +206,7 @@ public class Grid extends Entity implements Iterable<Agent> {
 	public Agent getAgent(int x, int y) {
 		if (isValidCoord(x, y))
 			return grid[x][y];
-		System.err.println("invalid Coord: " + x + "," + y);
+//		System.err.println("invalid Coord: " + x + "," + y);
 		return null;
 	}
 
