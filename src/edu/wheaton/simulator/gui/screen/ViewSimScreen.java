c286202fab8c01a89489ebd89ebc6e6137a98a78
brandon-garcia <brandon.garcia@my.wheaton.edu>
2013-04-22 06:28:39 -0500

    trying to make view sim screen more compact

--- src/edu/wheaton/simulator/gui/screen/ViewSimScreen.java
+++ src/edu/wheaton/simulator/gui/screen/ViewSimScreen.java
@@ -142,17 +142,12 @@ public class ViewSimScreen extends Screen {
 		c.gridy = 0;
 		c.ipadx = 600;
 		c.ipady = 600;
-		c.gridwidth = 1;
-		c.weighty = 1;
-		c.weightx = 1;
-		//c.insets = new Insets(5, 5, 5, 5);
 		rightPanel.add(gm.getGridPanel(), c);
 
 		c = new GridBagConstraints();
 		c.fill = GridBagConstraints.HORIZONTAL;
 		c.gridx = 0;
 		c.gridy = 1;
-		c.gridwidth = 1;
 		rightPanel.add(makeButtonPanel(), c);
 		
 		c = new GridBagConstraints();
