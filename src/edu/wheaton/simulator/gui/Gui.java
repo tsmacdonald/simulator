038d4f30eeb0a62bc695922cdaed59966784e8fd
brandon-garcia <brandon.garcia@my.wheaton.edu>
2013-04-22 06:37:58 -0500

    trying to fix menu bar

--- src/edu/wheaton/simulator/gui/Gui.java
+++ src/edu/wheaton/simulator/gui/Gui.java
@@ -162,10 +162,6 @@ public final class Gui {
 	
 	public static JMenu makeMenu(String name){
 		JMenu menu = new JMenu(name);
-		menu.setOpaque(true);
-		menu.setForeground(Color.white);
-		//menu.setBackground(Color.darkGray);
-		menu.setBorderPainted(false);
 		
 		menu.getPopupMenu().setBorder(
 			BorderFactory.createLineBorder(Color.black));
@@ -176,17 +172,11 @@ public final class Gui {
 	public static JMenuItem makeMenuItem(String name, ActionListener al){
 		JMenuItem menuItem = new JMenuItem(name);
 		menuItem.addActionListener(al);
-		//menuItem.setOpaque(true);
-		menuItem.setBackground(Color.gray);
-		menuItem.setForeground(Color.white);
 		return menuItem;
 	}
 	
 	private static JMenuBar makeMenuBar() {
 		JMenuBar menuBar = new JMenuBar();
-		menuBar.setOpaque(true);
-		menuBar.setForeground(Color.white);
-		menuBar.setBackground(Color.darkGray);
 
 		JMenu fileMenu = makeFileMenu();
 		//JMenu editMenu = makeEditMenu(sm);
